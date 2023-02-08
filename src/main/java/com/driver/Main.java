193fa7161b944953bdbfe437b8563ed85466f188
Arin <arin1998@gmail.com>
2023-02-08 13:23:33 +0530

    encapsulation solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.name="malvika"; //error : 'name' has private access in 'com.driver.RWOnly'
+        //System.out.println(rw.name);
+        rw.setName("Arin");
+        System.out.println(rw.getName());
+    }
 }
